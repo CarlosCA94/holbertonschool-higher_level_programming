641e5d86fd4067296ccb41a829ba1ba06ccfc80d
CarlosCA94 <cw.21_94@hotmail.com>
2023-07-16 20:45:05 -0700

    Reupload program - Imports functions and does some Maths

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
-from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
-result_1 = add(a, b)
-result_2 = sub(a, b)
-result_3 = mul(a, b)
-result_4 = div(a, b)
+    a = 10
+    b = 5
+    result_1 = add(a, b)
+    result_2 = sub(a, b)
+    result_3 = mul(a, b)
+    result_4 = div(a, b)
 print("{} + {} = {}".format(a, b, result_1))
 print("{} - {} = {}".format(a, b, result_2))
 print("{} * {} = {}".format(a, b, result_3))
