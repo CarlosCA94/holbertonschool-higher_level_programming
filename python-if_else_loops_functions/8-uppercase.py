608f928986e54d4c26d573aaecc2d71b0bea8d2d
CarlosCA94 <cw.21_94@hotmail.com>
2023-07-16 20:25:30 -0700

    Reupload - Program that checks uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-            print("{}".format(c), end="")
-        print("")
+    upper = ""
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            char = ord(char) - 32
+            upper += chr(char)
+        else:
+            upper += char
+    print("{}".format(upper))
