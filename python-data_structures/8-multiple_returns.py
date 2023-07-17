8a9ed1581d386d13802e96884be99bf9a04f9e7a
CarlosCA94 <cw.21_94@hotmail.com>
2023-07-16 21:30:36 -0700

    Reupload - Function that return a tuple

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+    len_str = len(sentence)
+    if len_str == 0:
+        first_character = None
+    else:
+        first_character = sentence[0]
+    return len_str, first_character
